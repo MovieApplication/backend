0a965b731e733344c07e40e657688cb6f1df23d8
yjkim-mtn <newy12@naver.com>
2023-04-24 17:16:10 +0900

    cors 오류해결

--- src/main/java/com/movie/movieapi/config/WebConfig.java
+++ src/main/java/com/movie/movieapi/config/WebConfig.java
@@ -1,28 +1,22 @@
 package com.movie.movieapi.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*");
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "access_token_key", "X-Requested-With", "Server Authorization", "Content-Type", "Content-Length", "Cache-Control"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3006")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
+    }
     }
 
-}
+
