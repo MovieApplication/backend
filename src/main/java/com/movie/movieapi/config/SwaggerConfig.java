3b81208336ffdce586962997938bc9aa4e994974
yjkim-mtn <newy12@naver.com>
2023-04-24 10:16:45 +0900

    springboot swagger 연동

---
+++ src/main/java/com/movie/movieapi/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.movie.movieapi.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Movie App",
+                description = "Movie API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/api/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Movie API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
--- src/main/java/com/movie/movieapi/controller/MovieController.java
+++ src/main/java/com/movie/movieapi/controller/MovieController.java
@@ -4,6 +4,7 @@ import com.movie.movieapi.dto.MovieDetailResponseDto;
 import com.movie.movieapi.dto.MovieResponseDto;
 import com.movie.movieapi.dto.SearchDto;
 import com.movie.movieapi.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,49 +19,34 @@ public class MovieController {
 
     private final MovieService movieService;
 
-    /**
-     * 실시간 인기 순위 영화 리스트 목록 조회
-     */
+    @Operation(summary = "실시간 인기 순위 영화 리스트 목록 조회", description = "실시간 인기 순위 영화 리스트 목록 조회를 합니다.")
     @GetMapping("/popular")
     public ResponseEntity<MovieResponseDto> selectPopularMovies(){
         return ResponseEntity.ok(movieService.selectPopularMovies());
     }
-
-    /**
-     * 평점 높은순으로 영화 목록 조회
-     */
+    @Operation(summary = "평점 높은순으로 영화 목록 조회", description = "평점 높은순으로 영화 목록 조회를 합니다.")
     @GetMapping("/vote-average")
     public ResponseEntity<MovieResponseDto> selectVoteAverageMovies(){
         return ResponseEntity.ok(movieService.selectVoteAverageMovies());
     }
-    /**
-     * 최신에 개봉한순으로 목록 조회(현재날짜기준)
-     */
+    @Operation(summary = "최신에 개봉한순으로 목록 조회(현재날짜기준)", description = "최신에 개봉한순으로 목록 조회(현재날짜기준)를 합니다.")
     @GetMapping("/release-date")
     public ResponseEntity<MovieResponseDto> selectReleaseDateMovies(){
         return ResponseEntity.ok(movieService.selectReleaseDateMovies());
     }
 
-    /**
-     * 년도별 영화 목록 조회
-     * @param searchDto
-     * @return
-     */
+    @Operation(summary = "년도별 영화 목록 조회", description = "년도별 영화 목록 조회를 합니다.")
     @GetMapping("/year")
     public ResponseEntity<MovieResponseDto> selectYearMovies(@ModelAttribute SearchDto searchDto){
         return ResponseEntity.ok(movieService.selectYearMovies(searchDto));
     }
 
-    /**
-     * 영화 세부 정보 조회
-     */
+    @Operation(summary = "영화 세부 정보 조회", description = "영화 세부 정보 조회를 합니다.")
     @GetMapping("/{movieId}")
     public ResponseEntity<MovieDetailResponseDto> selectMovie(@PathVariable("movieId")Long movieId){
         return ResponseEntity.ok(movieService.selectMovie(movieId));
     }
-    /**
-     * 해당 영화와 유사한 영화 목록조회
-     */
+    @Operation(summary = "해당 영화와 유사한 영화 목록조회", description = "해당 영화와 유사한 영화 목록조회를 합니다.")
     @GetMapping("/{movieId}/similar")
     public ResponseEntity<MovieResponseDto> selectSimilarMovie(@PathVariable("movieId")Long movieId){
         return ResponseEntity.ok(movieService.selectSimilarMovie(movieId));
