a25bf504ee1eb25eed1a39e0f7ea2ce7f35eed5e
yjkim-mtn <newy12@naver.com>
2023-05-19 17:52:22 +0900

    다른영화 중복리뷰 해제

--- src/main/java/com/movie/movieapi/repository/ReviewRepository.java
+++ src/main/java/com/movie/movieapi/repository/ReviewRepository.java
@@ -12,5 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReviewRepository extends MongoRepository<Review,String>{
     Page<Review> findAllByMovieId(Long movieId, Pageable pageable);
-    boolean existsByUserAndContentAndDelYn(User userInfo, String content, boolean delYn);
+
+    boolean existsByUserAndMovieIdAndContentAndDelYn(User userInfo, Long movieId, String content, boolean delYn);
 }
--- src/main/java/com/movie/movieapi/service/ReviewService.java
+++ src/main/java/com/movie/movieapi/service/ReviewService.java
@@ -34,7 +34,7 @@ public class ReviewService {
         User userInfo = userRepository.findByKakaoId(user.getKakaoId())
                 .orElseThrow(null);
 
-        boolean review = reviewRepository.existsByUserAndContentAndDelYn(userInfo,reviewInsertRequestDto.getContent(),false);
+        boolean review = reviewRepository.existsByUserAndMovieIdAndContentAndDelYn(userInfo,reviewInsertRequestDto.getMovieId(),reviewInsertRequestDto.getContent(),false);
 
         if(review){
             throw new NotFoundException("같은 내용으로 리뷰를 작성할 수 없습니다.");
